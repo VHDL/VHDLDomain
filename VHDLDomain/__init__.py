19f10ed6c933b0489d46867d44ef71a7a5b1f0cf
Patrick Lehmann <Paebbels@gmail.com>
2023-02-06 00:41:24 +0100

    Bumped version to v0.2.0. [skip ci]

--- VHDLDomain/__init__.py
+++ VHDLDomain/__init__.py
@@ -42,7 +42,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2016-2023, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.1.0"
+__version__ =   "0.2.0"
 
 from pathlib import Path
 from typing import Dict, Tuple, Any, Optional as Nullable, cast
